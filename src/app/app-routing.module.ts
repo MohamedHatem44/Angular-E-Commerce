f87ed8186bcabd08cb8a47837e94201202deb11d
Mohamed Hatem <mohamed.hatem157@example.com>
2024-03-29 16:16:56 +0200

    Hatem-Fetch User Data"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { CategoriesComponent } from './components/categories/categories.componen
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { CartComponent } from './components/cart/cart.component';
-import { AuthGuardService} from './guards/auth-guard.guard';
+import { AuthGuardService } from './guards/auth-guard.guard';
 import { AdminGuardService } from './guards/admin-guard.guard';
 
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
@@ -23,16 +23,36 @@ const routes: Routes = [
   { path: 'products', component: ProductsComponent },
   { path: 'categories', component: CategoriesComponent },
   { path: 'brands', component: BrandsComponent },
-  { path: 'cart', component: CartComponent ,canActivate: [AuthGuardService], },
-  { path: 'usersDashboard', component: UsersComponent ,canActivate: [AuthGuardService,AdminGuardService],}, //edited remove it
-  { path: 'userprofile', component: UserProfileComponent,canActivate: [AuthGuardService] },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuardService] },
+  {
+    path: 'usersDashboard',
+    component: UsersComponent,
+    canActivate: [AuthGuardService, AdminGuardService],
+  }, //edited remove it
+  {
+    path: 'userprofile/:id',
+    component: UserProfileComponent,
+    canActivate: [AuthGuardService],
+  },
   { path: 'cart', component: CartComponent },
-  { path: 'usersDashboard', component: UsersComponent,canActivate: [AuthGuardService,AdminGuardService] }, //edited remove it
+  {
+    path: 'usersDashboard',
+    component: UsersComponent,
+    canActivate: [AuthGuardService, AdminGuardService],
+  }, //edited remove it
   { path: 'users/login', component: LoginComponent }, //login
   { path: 'users/register', component: RegisterComponent }, //register
-  { path: 'addProduct', component: ProductsFormComponent ,canActivate: [AuthGuardService,AdminGuardService],},
+  {
+    path: 'addProduct',
+    component: ProductsFormComponent,
+    canActivate: [AuthGuardService, AdminGuardService],
+  },
   { path: 'products/:id', component: ProductsFormComponent },
-  { path: 'productsDashboard', component: ProductsDashboardComponent ,canActivate: [AuthGuardService,AdminGuardService], },
+  {
+    path: 'productsDashboard',
+    component: ProductsDashboardComponent,
+    canActivate: [AuthGuardService, AdminGuardService],
+  },
   { path: 'productsDetails/:id', component: ProductsDetailsComponent },
   { path: '**', component: NotFoundComponent },
 ];

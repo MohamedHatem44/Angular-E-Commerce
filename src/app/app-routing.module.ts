df81c357a9f53a76acbfddbe83178310e7a885a2
Mohamed Hatem <mohamed.hatem157@example.com>
2024-03-31 23:27:47 +0200

    Hatem- Edit app-routing"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,35 +24,14 @@ const routes: Routes = [
   { path: 'categories', component: CategoriesComponent },
   { path: 'brands', component: BrandsComponent },
   { path: 'cart', component: CartComponent, canActivate: [AuthGuardService] },
-  {
-    path: 'usersDashboard',
-    component: UsersComponent,
-    canActivate: [AuthGuardService, AdminGuardService],
-  }, //edited remove it
-  {
-    path: 'userprofile/:id',
-    component: UserProfileComponent,
-    canActivate: [AuthGuardService],
-  },
+  { path: 'usersDashboard', component: UsersComponent, canActivate: [AuthGuardService, AdminGuardService] },
+  { path: 'userprofile/:id', component: UserProfileComponent, canActivate: [AuthGuardService] },
   { path: 'cart', component: CartComponent },
-  {
-    path: 'usersDashboard',
-    component: UsersComponent,
-    canActivate: [AuthGuardService, AdminGuardService],
-  }, //edited remove it
-  { path: 'users/login', component: LoginComponent }, //login
-  { path: 'users/register', component: RegisterComponent }, //register
-  {
-    path: 'addProduct',
-    component: ProductsFormComponent,
-    canActivate: [AuthGuardService, AdminGuardService],
-  },
+  { path: 'users/login', component: LoginComponent },
+  { path: 'users/register', component: RegisterComponent },
+  { path: 'addProduct', component: ProductsFormComponent, canActivate: [AuthGuardService, AdminGuardService] },
   { path: 'products/:id', component: ProductsFormComponent },
-  {
-    path: 'productsDashboard',
-    component: ProductsDashboardComponent,
-    canActivate: [AuthGuardService, AdminGuardService],
-  },
+  { path: 'productsDashboard', component: ProductsDashboardComponent, canActivate: [AuthGuardService, AdminGuardService] },
   { path: 'productsDetails/:id', component: ProductsDetailsComponent },
   { path: '**', component: NotFoundComponent },
 ];

98fc08708022ee3905ebbc4be4cccb825aaff854
Mohamed Hatem <mohamed.hatem157@example.com>
2024-04-01 23:23:26 +0200

    Hatem-Add New Category

--- src/app/components/admin/admin-add-category/admin-add-category.component.ts
+++ src/app/components/admin/admin-add-category/admin-add-category.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Category } from 'src/app/models/category';
 import { CategoryService } from 'src/app/services/category.service';
 
@@ -10,7 +10,10 @@ import { CategoryService } from 'src/app/services/category.service';
 })
 export class AdminAddCategoryComponent implements OnInit {
   categories: Category[] = [];
-  newCategory: any = { name: '', image: null };
+  backendErrors: boolean = false;
+  // newCategory: any = { name: '', image: null };
+
+  // addCategoryForm: FormGroup;
   /*-----------------------------------------------------------------*/
   // Ctor
   constructor(private _CategoryService: CategoryService) {}
@@ -27,30 +30,44 @@ export class AdminAddCategoryComponent implements OnInit {
     );
   }
   /*-----------------------------------------------------------------*/
-  addCategoryForm: FormGroup = new FormGroup({
+  addCategoryForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(15), Validators.pattern('^[a-zA-Z]*$')]),
     image: new FormControl('', [Validators.required]),
   });
-
   /*-----------------------------------------------------------------*/
   // Create Category
-  createCategory(): void {
-    this._CategoryService.createCategory(this.newCategory).subscribe(
-      (response: any) => {
-        this.categories.push(response.data);
-        this.newCategory = { name: '', image: null };
-      },
-      (error: any) => {
-        console.error('Error creating category:', error);
-      }
-    );
-  }
+  // createCategory(): void {
+  //   this._CategoryService.createCategory(this.newCategory).subscribe(
+  //     (response: any) => {
+  //       this.categories.push(response.data);
+  //       this.newCategory = { name: '', image: null };
+  //     },
+  //     (error: any) => {
+  //       console.error('Error creating category:', error);
+  //     }
+  //   );
+  // }
   /*-----------------------------------------------------------------*/
   createCategoryBtn() {
     if (this.addCategoryForm.invalid) {
       return;
     }
-    console.log(this.addCategoryForm.controls['name'].value);
-    console.log(this.addCategoryForm.controls['image'].value);
+
+    const newCategory: any = {
+      name: this.addCategoryForm.controls['name'].value,
+      image: this.addCategoryForm.controls['image'].value,
+    };
+
+    this._CategoryService.createCategory(newCategory).subscribe(
+      (response: any) => {
+        alert('Category created successfully.');
+        this.addCategoryForm.reset();
+      },
+      (error: any) => {
+        alert('An error occurred while creating the category. Please try again.');
+        this.backendErrors = true;
+      }
+    );
   }
 }
+/*-----------------------------------------------------------------*/

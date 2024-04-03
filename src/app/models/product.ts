dacb7141e4ca4e2079bc9113f7e26c56203c60d6
Mohamed Hatem <mohamed.hatem157@example.com>
2024-04-03 04:43:57 +0200

    Hatem-admin-products-dashboard

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,12 +1,23 @@
 export interface Product {
-  imageCover: string;
+  _id: string;
   title: string;
-  category: {
+  description: string;
+  quantity: number;
+  sold?: number;
+  price: number;
+  priceAfterDiscount?: number;
+  colors?: string[];
+  image?: string;
+  images: string;
+  category?: {
     name: string;
   };
-  price: number;
-  ratingsAverage: number;
-  _id: string;
-  description: string;
-  images: string;
+  subcategories?: {
+    name: string;
+  };
+  brand?: {
+    name: string;
+  };
+  ratingsAverage?: number;
+  ratingsQuantity?: number;
 }
